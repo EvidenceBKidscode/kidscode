e92c698d15f48646530052e5c9245590aad60813
Jean-Patrick Guerrero <jeanpatrick.guerrero@gmail.com>
2017-06-23 19:34:24 +0200

    update default settings

--- src/defaultsettings.cpp
+++ src/defaultsettings.cpp
@@ -324,9 +324,9 @@ void set_default_settings(Settings *settings)
 	settings->setDefault("emergequeue_limit_diskonly", "32");
 	settings->setDefault("emergequeue_limit_generate", "32");
 	settings->setDefault("num_emerge_threads", "1");
-	settings->setDefault("secure.enable_security", "true");
-	settings->setDefault("secure.trusted_mods", "kidscode_acl,kidscode_utility");
-	settings->setDefault("secure.http_mods", "kidscode_acl,kidsbot,player_menu");
+	settings->setDefault("secure.enable_security", "false");
+	settings->setDefault("secure.trusted_mods", "kidscode_acl,kidscode_utility,kidsbot");
+	settings->setDefault("secure.http_mods", "kidscode_utility,kidscode_acl,kidsbot,player_menu");
 
 	// Physics
 	settings->setDefault("movement_acceleration_default", "3");
