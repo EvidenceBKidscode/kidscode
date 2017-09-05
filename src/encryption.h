1c33d0b1a84cd127c8bad21c7b2bcb6daef7e4e3
Irrlick <irrlick@gmail.com>
2017-09-05 17:14:01 +0200

    Added encrytion tag constants

--- src/encryption.h
+++ src/encryption.h
@@ -8,11 +8,17 @@
 	#define ENCRYPTION_KEY "01234567012345670123456701234567"
 #endif
 
+#define ENCRYPTION_TAG_0 4
+#define ENCRYPTION_TAG_1 1
+#define ENCRYPTION_TAG_2 6
+#define ENCRYPTION_TAG_3 5
 
 inline const bool isCrypted(const char *text, int size)
 {
-	return size >= 4 && text[size - 4] == 4 && text[size - 3] == 1 && 
-		text[size - 2] == 6 && text[size - 3] == 5;
+	return size >= 4 && text[size - 4] == ENCRYPTION_TAG_0 && 
+		text[size - 3] == ENCRYPTION_TAG_1 && 
+		text[size - 2] == ENCRYPTION_TAG_2 && 
+		text[size - 3] == ENCRYPTION_TAG_3;
 }
 
 
@@ -37,6 +43,10 @@ inline char *encrypt(char *text, int &size)
 	char *encrypted_text = new char[size + 4];
 	AES_ECB_encrypt((const uint8_t*)text, (const uint8_t*)ENCRYPTION_KEY, 
 		(uint8_t*)encrypted_text, size);
+	text[size] = ENCRYPTION_TAG_0;
+	text[size + 1] = ENCRYPTION_TAG_1;
+	text[size + 2] = ENCRYPTION_TAG_2;
+	text[size + 3] = ENCRYPTION_TAG_3;
 	size += 4;
 	delete[] text;
 
