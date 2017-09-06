6f71ba32a836a279106da2ce46bd9ba7c7903b2b
Irrlick <irrlick@gmail.com>
2017-09-06 07:25:07 +0200

    Simplified makeKey

--- src/encryption.h
+++ src/encryption.h
@@ -14,18 +14,23 @@
 #define ENCRYPTION_TAG_3 5
 
 
-static inline void getName(const char *file_name, const char *&name, int &length)
+static inline const char *getName(const char *file_name)
 {
-	name = file_name;
+	const char *name = file_name;
+
 	for (int i=0; file_name[i]; ++i) {
 		if (file_name[i] == '/' || name[i] == '\\')
 			name = file_name + i + 1;
 	}
+	
+	return name;
 }
 
 
-static inline void makeKey(unsigned char *key, const unsigned char *name, unsigned int length)
+static inline void makeKey(unsigned char *key, const unsigned char *name)
 {
+	unsigned int length = strlen((const char *)name);
+
 	for (unsigned i = 0; i < ENCRYPTION_KEY_SIZE; ++i) {
 		key[i] = ENCRYPTION_KEY[i] ^ (name[i % length] * 1046527 + key[(name[i % length] * 16769023) & 31] & 255);
 	}
@@ -47,12 +52,13 @@ static inline char *decrypt(char *text, int &size, const char *name)
 		return text;
 
 	unsigned char key[ENCRYPTION_KEY_SIZE];
-	makeKey(key, (const unsigned char *)name, strlen(name));
+	makeKey(key, (const unsigned char *)name);
 
-	size -= 4;
 	char *decrypted_text = new char[size];
+	size -= 4;
 	AES_ECB_decrypt((const unsigned char *)text, (const unsigned char *)ENCRYPTION_KEY, 
 		(unsigned char *)decrypted_text, size);
+	decrypted_text[size] = 0;
 	delete[] text;
 }
 
@@ -63,11 +69,11 @@ static inline char *encrypt(char *text, int &size, const char *name)
 		return text;
 
 	unsigned char key[ENCRYPTION_KEY_SIZE];
-	makeKey(key, (const unsigned char *)name, strlen(name));
+	makeKey(key, (const unsigned char *)name);
 
 	char *encrypted_text = new char[size + 4];
-	AES_ECB_encrypt((const uint8_t*)text, (const uint8_t*)ENCRYPTION_KEY, 
-		(uint8_t*)encrypted_text, size);
+	AES_ECB_encrypt((const unsigned char *)text, (const unsigned char *)ENCRYPTION_KEY, 
+		(unsigned char *)encrypted_text, size);
 	text[size] = ENCRYPTION_TAG_0;
 	text[size + 1] = ENCRYPTION_TAG_1;
 	text[size + 2] = ENCRYPTION_TAG_2;
