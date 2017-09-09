af323c29e84e1a08eccd8d1a1217a6a3ea8f517f
Irrlick <irrlick@gmail.com>
2017-09-09 10:15:41 +0200

    Changed table and field names in md5.sh

--- util/md5/md5.sh
+++ util/md5/md5.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-find . -name "*.lua" -exec md5sum '{}' \; | awk 'BEGIN{FS="  ./";OFS=""}{print "replace into DATABASE.TABLE(Filename,Signature) values(\"",$2,"\",\"",$1,"\");"}'
+find . -name "*.lua" -exec md5sum '{}' \; \
+| awk 'BEGIN{FS="  ./";OFS=""}{print "update maps set mods_hash_md5 = \"",$1,"\" where url_download = \"http://assets.kidscode.com/master/",$2,"\";"}'
