0a131e9449c3a15d8b1492826b6fbfe19b52f7ad
Pierre-Yves Rollo <dev@pyrollo.com>
2019-05-20 11:46:21 +0200

    New liquid display logic (including sources)

--- src/map.cpp
+++ src/map.cpp
@@ -71,7 +71,7 @@ Map::Map(std::ostream &dout, IGameDef *gamedef):
 	m_gamedef(gamedef),
 	m_nodedef(gamedef->ndef())
 {
-	//m_liquid_logic = new LiquidLogicNone(this, gamedef);
+//	m_liquid_logic = new LiquidLogicNone(this, gamedef);
 	m_liquid_logic = new LiquidLogicPreserve(this, gamedef);
 //	m_liquid_logic = new LiquidLogicClassic(this, gamedef);
 }
