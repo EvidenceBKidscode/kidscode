c783b388210caff1d79047db0a2da7a2f6192bfb
Pierre-Yves Rollo <dev@pyrollo.com>
2019-06-27 17:11:00 +0200

    Clip text, better alignment and bug fixes

--- src/gui/guiText.cpp
+++ src/gui/guiText.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 using namespace irr::gui;
 #include "guiText.h"
-
+#include "fontengine.h"
 #include <SColor.h>
 
 //! constructor
@@ -31,12 +31,11 @@ void GUIText::drawline(
 	std::vector<GUIText::word>& words,
 	int halign)
 {
-	core::rect<s32> word_rect = rectangle;
 
 	s32 textwidth = 0;
 	s32 textheight = 0;
-	s32 startx = 0;
-	s32 extraspace = 0;
+	float x = 0;
+	float extraspace = 0;
 
 	for (auto & word : words) {
 		textwidth += word.dimension.Width;
@@ -44,26 +43,29 @@ void GUIText::drawline(
 			textheight = word.dimension.Height;
 	}
 
+	rectangle.LowerRightCorner.Y = rectangle.UpperLeftCorner.Y + textheight;
+
 	switch(halign)
 	{
 		case center:
-			startx = (rectangle.getWidth() - textwidth) / 2;
+			x = (rectangle.getWidth() - textwidth) / 2;
 			break;
 		case justify:
 			if (words.size() > 1)
-				extraspace = (rectangle.getWidth() - textwidth) / (words.size() - 1);
+				extraspace = ((float)(rectangle.getWidth() - textwidth)) / (words.size() - 1);
 			break;
 		case right:
-			startx = (rectangle.getWidth() - textwidth);
+			x = (rectangle.getWidth() - textwidth);
 	}
 
-	word_rect += core::position2d<s32>(startx, 0);
-
 	for (auto & word : words) {
-		word.font->draw(word.text, word_rect,
-			irr::video::SColor(255, 255, 255, 255),
-			false, true, 0); //&local_clip_rect);
-		word_rect += core::position2d<s32>(word.dimension.Width + extraspace, 0);
+		core::rect<s32> c = rectangle + core::position2d<s32>(x, 0);
+		if (c.isRectCollided(AbsoluteClippingRect)) {
+			word.font->draw(word.text, c,
+				irr::video::SColor(255, 255, 255, 255),
+				false, true, &AbsoluteClippingRect);
+		}
+		x += word.dimension.Width + extraspace;
 	}
 	words.clear();
 	rectangle += core::position2d<s32>(0, textheight);
@@ -78,7 +80,12 @@ void GUIText::draw()
 	IGUISkin* skin = Environment->getSkin();
 	if (!skin)
 		return;
-	IGUIFont* font = skin->getFont();
+//	IGUIFont* font = skin->getFont();
+
+//	skin->draw2DRectangle (this, irr::video::SColor(255, 255, 0, 0), AbsoluteRect);
+//	skin->draw2DRectangle (this, irr::video::SColor(255, 0, 255, 0), AbsoluteClippingRect);
+
+	IGUIFont* font = g_fontengine->getFont(18);
 	if (!font)
 		return;
 
@@ -99,19 +106,18 @@ void GUIText::draw()
 
 		for (auto & word : words)
 		{
-			// Remove first space of each line
-			if (words_to_draw.size() == 0 && word[0] == L' ') word.erase(0);
-
-			GUIText::word word_to_draw(font->getDimension(word.c_str()), word, font);
-
-			if (word_to_draw.dimension.Width + width <= linewidth)
+			if (font->getDimension(word.c_str()).Width + width > linewidth)
 			{
-				words_to_draw.push_back(word_to_draw);
-				width += word_to_draw.dimension.Width;
-			} else {
+				// TODO: Split very large words
 				drawline(line_rect, words_to_draw, justify);
 				width = 0;
 			}
+			// Remove first space of each line
+			if (words_to_draw.size() == 0 && (word[0] == L' ')) word.erase(0);
+
+			GUIText::word word_to_draw(font->getDimension(word.c_str()), word, font);
+			words_to_draw.push_back(word_to_draw);
+			width += word_to_draw.dimension.Width;
 		}
 
 		if (! words_to_draw.empty())
