423c78ab771de0e2b0b79aced7e9d13250eaf336
Jean-Patrick Guerrero <jeanpatrick.guerrero@gmail.com>
2019-07-12 01:52:55 +0200

    Set AZERTY keyboard layout by default

--- src/defaultsettings.cpp
+++ src/defaultsettings.cpp
@@ -61,15 +61,15 @@ void set_default_settings(Settings *settings)
 
 	// Keymap
 	settings->setDefault("remote_port", "30000");
-	settings->setDefault("keymap_forward", "KEY_KEY_W");
+	settings->setDefault("keymap_forward", "KEY_KEY_Z");
 	settings->setDefault("keymap_autoforward", "");
 	settings->setDefault("keymap_backward", "KEY_KEY_S");
-	settings->setDefault("keymap_left", "KEY_KEY_A");
+	settings->setDefault("keymap_left", "KEY_KEY_Q");
 	settings->setDefault("keymap_right", "KEY_KEY_D");
 	settings->setDefault("keymap_jump", "KEY_SPACE");
 	settings->setDefault("keymap_sneak", "KEY_LSHIFT");
 	settings->setDefault("keymap_drop", "KEY_KEY_Q");
-	settings->setDefault("keymap_zoom", "KEY_KEY_Z");
+	settings->setDefault("keymap_zoom", "KEY_KEY_X");
 	settings->setDefault("keymap_inventory", "KEY_KEY_I");
 	settings->setDefault("keymap_special1", "KEY_KEY_E");
 	settings->setDefault("keymap_chat", "KEY_KEY_T");
