3bd02c57786d07f27663eccce8b25edc76b0b571
Pierre-Yves Rollo <dev@pyrollo.com>
2019-07-16 15:39:45 +0200

    Code style

--- src/gui/guiText.h
+++ src/gui/guiText.h
@@ -26,154 +26,153 @@ class Client;
 
 class ParsedText
 {
-	public:
-		ParsedText(const wchar_t* text);
-		~ParsedText();
+public:
+	ParsedText(const wchar_t* text);
+	~ParsedText();
 
-		enum ElementType { ELEMENT_TEXT, ELEMENT_SEPARATOR, ELEMENT_IMAGE, ELEMENT_ITEM };
-		enum BackgroundType { BACKGROUND_NONE, BACKGROUND_COLOR };
-		enum FloatType { FLOAT_NONE, FLOAT_RIGHT, FLOAT_LEFT };
-		enum HalignType { HALIGN_CENTER, HALIGN_LEFT, HALIGN_RIGHT, HALIGN_JUSTIFY };
-		enum ValignType { VALIGN_MIDDLE, VALIGN_TOP, VALIGN_BOTTOM };
+	enum ElementType { ELEMENT_TEXT, ELEMENT_SEPARATOR, ELEMENT_IMAGE, ELEMENT_ITEM };
+	enum BackgroundType { BACKGROUND_NONE, BACKGROUND_COLOR };
+	enum FloatType { FLOAT_NONE, FLOAT_RIGHT, FLOAT_LEFT };
+	enum HalignType { HALIGN_CENTER, HALIGN_LEFT, HALIGN_RIGHT, HALIGN_JUSTIFY };
+	enum ValignType { VALIGN_MIDDLE, VALIGN_TOP, VALIGN_BOTTOM };
 
-		typedef std::unordered_map<std::string, std::string> StyleList;
-		typedef std::unordered_map<std::string, std::string> AttrsList;
+	typedef std::unordered_map<std::string, std::string> StyleList;
+	typedef std::unordered_map<std::string, std::string> AttrsList;
 
-		struct Tag {
-			std::string name;
-			AttrsList attrs;
-			StyleList style;
-		};
+	struct Tag {
+		std::string name;
+		AttrsList attrs;
+		StyleList style;
+	};
 
-		struct Element {
-			std::list<Tag*> tags;
-			ElementType type;
-			core::stringw text;
+	struct Element {
+		std::list<Tag*> tags;
+		ElementType type;
+		core::stringw text;
 
-			core::dimension2d<u32> dim;
-			core::position2d<s32> pos;
-			FloatType floating = FLOAT_NONE;
+		core::dimension2d<u32> dim;
+		core::position2d<s32> pos;
+		FloatType floating = FLOAT_NONE;
 
-			ValignType valign;
-			gui::IGUIFont* font;
-			irr::video::SColor color;
-			irr::video::SColor hovercolor;
+		ValignType valign;
+		gui::IGUIFont* font;
+		irr::video::SColor color;
+		irr::video::SColor hovercolor;
 
-			// img & item specific attributes
-			std::string name;
-			ItemRotationKind rotation = IT_ROT_NONE;
-			s32 margin = 10;
+		// img & item specific attributes
+		std::string name;
+		ItemRotationKind rotation = IT_ROT_NONE;
+		s32 margin = 10;
 
-			void setStyle(StyleList &style);
-		};
+		void setStyle(StyleList &style);
+	};
 
-		struct Paragraph {
-			std::vector<Element> elements;
-			HalignType halign;
-			s32 margin = 10;
+	struct Paragraph {
+		std::vector<Element> elements;
+		HalignType halign;
+		s32 margin = 10;
 
-			void setStyle(StyleList &style);
-		};
+		void setStyle(StyleList &style);
+	};
 
-		std::vector<Paragraph> m_paragraphs;
+	std::vector<Paragraph> m_paragraphs;
 
-		// Element style
-		s32 margin = 3;
-		ValignType valign = VALIGN_TOP;
-		BackgroundType background_type = BACKGROUND_NONE;
-		irr::video::SColor background_color;
+	// Element style
+	s32 margin = 3;
+	ValignType valign = VALIGN_TOP;
+	BackgroundType background_type = BACKGROUND_NONE;
+	irr::video::SColor background_color;
 
-		Tag m_root_tag;
+	Tag m_root_tag;
 
-	protected:
+protected:
 
-		// Parser functions
-		void globalTag(ParsedText::AttrsList& attrs);
-		void enterElement(ElementType type);
-		void endElement();
-		void enterParagraph();
-		void endParagraph();
-		void pushChar(wchar_t c);
-		ParsedText::Tag* newTag(std::string name, AttrsList attrs);
-		ParsedText::Tag* openTag(std::string, AttrsList attrs);
-		bool closeTag(std::string name);
-		u32 parseTag(const wchar_t* text, u32 cursor);
-		void parse(const wchar_t* text);
+	// Parser functions
+	void globalTag(ParsedText::AttrsList& attrs);
+	void enterElement(ElementType type);
+	void endElement();
+	void enterParagraph();
+	void endParagraph();
+	void pushChar(wchar_t c);
+	ParsedText::Tag* newTag(std::string name, AttrsList attrs);
+	ParsedText::Tag* openTag(std::string, AttrsList attrs);
+	bool closeTag(std::string name);
+	u32 parseTag(const wchar_t* text, u32 cursor);
+	void parse(const wchar_t* text);
 
-		std::vector<Tag *> m_tags;
-		std::list<Tag *> m_active_tags;
+	std::vector<Tag *> m_tags;
+	std::list<Tag *> m_active_tags;
 
-		// Current values
-		StyleList m_style;
-		Element* m_element;
-		Paragraph* m_paragraph;
+	// Current values
+	StyleList m_style;
+	Element* m_element;
+	Paragraph* m_paragraph;
 };
 
 
 class TextDrawer
 {
-	public:
-		TextDrawer(
-			const wchar_t* text,
-			Client *client,
-			gui::IGUIEnvironment* environment,
-			ISimpleTextureSource *tsrc);
+public:
+	TextDrawer(
+		const wchar_t* text,
+		Client *client,
+		gui::IGUIEnvironment* environment,
+		ISimpleTextureSource *tsrc);
 
-		void place(s32 width);
-		inline s32 getHeight() { return m_height; };
-		s32 getVoffset(s32 height);
-		void draw(core::rect<s32> dest_rect, core::position2d<s32> dest_offset);
-		ParsedText::Element* getElementAt(core::position2d<s32> pos);
-		ParsedText::Tag* m_hovertag;
+	void place(s32 width);
+	inline s32 getHeight() { return m_height; };
+	s32 getVoffset(s32 height);
+	void draw(core::rect<s32> dest_rect, core::position2d<s32> dest_offset);
+	ParsedText::Element* getElementAt(core::position2d<s32> pos);
+	ParsedText::Tag* m_hovertag;
 
-	protected:
+protected:
 
-		struct RectWithMargin {
-			core::rect<s32> rect;
-			s32 margin;
-		};
+	struct RectWithMargin {
+		core::rect<s32> rect;
+		s32 margin;
+	};
 
-		ParsedText m_text;
-		Client *m_client;
-		gui::IGUIEnvironment* m_environment;
-		s32 m_height;
-		std::vector<RectWithMargin> m_floating;
-
+	ParsedText m_text;
+	Client *m_client;
+	gui::IGUIEnvironment* m_environment;
+	s32 m_height;
+	std::vector<RectWithMargin> m_floating;
 };
 
 class GUIText : public gui::IGUIElement
 {
-	public:
-		//! constructor
-		GUIText(
-			const wchar_t* text,
-			gui::IGUIEnvironment* environment,
-			gui::IGUIElement* parent,
-			s32 id,
-			const core::rect<s32>& rectangle,
-			Client *client,
-			ISimpleTextureSource *tsrc);
+public:
+	//! constructor
+	GUIText(
+		const wchar_t* text,
+		gui::IGUIEnvironment* environment,
+		gui::IGUIElement* parent,
+		s32 id,
+		const core::rect<s32>& rectangle,
+		Client *client,
+		ISimpleTextureSource *tsrc);
 
-		//! destructor
-		virtual ~GUIText();
+	//! destructor
+	virtual ~GUIText();
 
-		//! draws the element and its children
-		virtual void draw();
+	//! draws the element and its children
+	virtual void draw();
 
-		bool OnEvent(const SEvent& event);
+	bool OnEvent(const SEvent& event);
 
-	protected:
+protected:
 
-		// GUI members
-		Client *m_client;
-		gui::IGUIScrollBar *m_vscrollbar;
-		TextDrawer m_drawer;
+	// GUI members
+	Client *m_client;
+	gui::IGUIScrollBar *m_vscrollbar;
+	TextDrawer m_drawer;
 
-		// Positionning
-		u32 m_scrollbar_width;
-		core::rect<s32> m_display_text_rect;
-		core::position2d<s32> m_text_scrollpos;
+	// Positionning
+	u32 m_scrollbar_width;
+	core::rect<s32> m_display_text_rect;
+	core::position2d<s32> m_text_scrollpos;
 
-		ParsedText::Element *getElementAt(s32 X, s32 Y);
-		void checkHover(s32 X, s32 Y);
+	ParsedText::Element *getElementAt(s32 X, s32 Y);
+	void checkHover(s32 X, s32 Y);
 };
