587ed979ad25f9e907ee45745ce68229759ca78d
Jean-Patrick Guerrero <jeanpatrick.guerrero@gmail.com>
2019-07-30 17:07:47 +0200

    Pause menu tweaks

--- src/client/game.cpp
+++ src/client/game.cpp
@@ -1047,7 +1047,7 @@ static inline void create_formspec_menu(GUIFormSpecMenu **cur_formspec,
 #ifdef __ANDROID__
 #define SIZE_TAG "size[11,5.5]"
 #else
-#define SIZE_TAG "size[18.4,1.1,true]" // Fixed size on desktop
+#define SIZE_TAG "size[18.2,1.1,true]" // Fixed size on desktop
 #endif
 
 /******************************************************************************/
@@ -4870,7 +4870,7 @@ void Game::showPauseMenu()
 
 	f32 xpos = 0.0f;
 
-	os << "anchor[0.5,4.35]";
+	os << "anchor[0.5,4.4]";
 
 	os << "label[1,0;" << strgettext("PAUSE") << "]";
 
@@ -4889,17 +4889,17 @@ void Game::showPauseMenu()
 	os << "button_exit["<< (xpos+=2.9f) << ",0;3,0.5;btn_key_config;"
 	   << strgettext("Change Keys")  << "]";
 
-	os << "label[" << (xpos+=3.6f) << ",0;" << strgettext("Mouse sensitivity") << "]";
 	std::string mouse_sensitivity = std::to_string(g_settings->getFloat("mouse_sensitivity") * 500.0f);
-	os << "scrollbar[" << (xpos-=.6f) << ",0.7;3,0.6;horizontal;sbr_mouse_sensitivity;" << mouse_sensitivity << "]";
+	os << "scrollbar[" << (xpos+=3.f) << ",0.67;3,0.7;horizontal;sbr_mouse_sensitivity;" << mouse_sensitivity << "]";
+	os << "label[" << (xpos+=.1f) << ",0;" << strgettext("Mouse sensitivity") << "]";
 
-	os << "label[" << (xpos+=4.0f) << ",0;" << strgettext("Viewing range") << "]";
 	std::string viewing_range = std::to_string((g_settings->getFloat("viewing_range") / 100.0f) * 250.0f);
-	os << "scrollbar[" << (xpos-=.8f) << ",0.7;3,0.6;horizontal;sbr_viewing_range;" << viewing_range << "]";
+	os << "scrollbar[" << (xpos+=3.f) << ",0.67;3,0.7;horizontal;sbr_viewing_range;" << viewing_range << "]";
+	os << "label[" << (xpos+=.1f) << ",0;" << strgettext("Viewing range") << "]";
 
-	os << "label[" << (xpos+=4.0f) << ",0;" << strgettext("GUI scaling") << "]";
 	std::string gui_scaling = std::to_string(g_settings->getFloat("gui_scaling") * 500.0f);
-	os << "scrollbar[" << (xpos-=.8f) << ",0.7;3,0.6;horizontal;sbr_gui_scaling;" << gui_scaling << "]";
+	os << "scrollbar[" << (xpos+=3.f) << ",0.67;3,0.7;horizontal;sbr_gui_scaling;" << gui_scaling << "]";
+	os << "label[" << (xpos+=.1f) << ",0;" << strgettext("GUI scaling") << "]";
 
 	/* Create menu */
 	/* Note: FormspecFormSource and LocalFormspecHandler  *
