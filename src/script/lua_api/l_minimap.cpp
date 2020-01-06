e80b2f7de185548d2b5093303ab73d1860125687
Pierre-Yves Rollo <dev@pyrollo.com>
2020-01-06 15:27:14 +0100

    Avoid a warning in compilation

--- src/script/lua_api/l_minimap.cpp
+++ src/script/lua_api/l_minimap.cpp
@@ -98,7 +98,7 @@ int LuaMinimap::l_set_mode(lua_State *L)
 	LuaMinimap *ref = checkobject(L, 1);
 	Minimap *m = getobject(ref);
 
-	s32 mode = lua_tointeger(L, 2);
+	u32 mode = lua_tointeger(L, 2);
 	if (mode < 0 || mode >= m->getMaxModeIndex()) {
 		return 0;
 	}
