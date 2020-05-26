b0727d84a3b6c153f95b0321a7d6fdf2b3c5c4f3
Pierre-Yves Rollo <dev@pyrollo.com>
2020-05-26 12:09:39 +0200

    Changed slideshow image format from PNG to JPG

--- builtin/mainmenu/tab_slideshow.lua
+++ builtin/mainmenu/tab_slideshow.lua
@@ -1,6 +1,6 @@
 function get_formspec_slideshow(tabview, name, tabdata)
 	return "animated_image[0,0;14,9.5;;" ..
-		core.formspec_escape(defaulttexturedir .. "slideshow.png") .. ";3;3000]"
+		core.formspec_escape(defaulttexturedir .. "slideshow.jpg") .. ";3;3000]"
 end
 
 return {
