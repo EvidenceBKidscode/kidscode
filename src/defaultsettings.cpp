6c6320e5ef827cead2191067c00b3c259788cfdb
Jean-Patrick Guerrero <jeanpatrick.guerrero@gmail.com>
2020-06-02 15:01:40 +0200

    Disable menu_clouds by default

--- src/defaultsettings.cpp
+++ src/defaultsettings.cpp
@@ -214,7 +214,7 @@ void set_default_settings(Settings *settings)
 	settings->setDefault("fall_bobbing_amount", "0.0"); // KIDSCODE Changed
 	settings->setDefault("enable_3d_clouds", "true");
 	settings->setDefault("cloud_radius", "12");
-	settings->setDefault("menu_clouds", "true");
+	settings->setDefault("menu_clouds", "false");
 	settings->setDefault("opaque_water", "false");
 	settings->setDefault("console_height", "1.0"); // KIDSCODE Changed
 	settings->setDefault("console_color", "(0,0,0)");
