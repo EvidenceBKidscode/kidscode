75f4b8ae0edc8ae8a043d05b5b0dbaf76ce8e354
Jean-Patrick Guerrero <jeanpatrick.guerrero@gmail.com>
2020-06-11 14:47:12 +0200

    Format text better

--- builtin/mainmenu/tab_help.lua
+++ builtin/mainmenu/tab_help.lua
@@ -8,45 +8,32 @@ local chapters = {
 	{"Les différents modes de jeu", [[
 		<b>Partie solo – Choix de la carte I</b>
 
-		Ce mode de jeu vous permet de jouer, d’explorer, de construire ou réaliser des simulations d’aléas
-		climatiques seul(e) sur une carte en 3D.
-		Dès lors que vous avez commandé une carte via le GAR ou Eduthèque elle s’affiche dans la liste des
-		cartes de l’onglet « Partie solo ». La carte passe par plusieurs états avant de pouvoir être lancée.
-		Si la carte est « <style color=orange>en préparation</style> » cela veut dire que les services IGN sont en train de la générer avec
-		toutes les données et options que vous avez choisi sur le site Minetest à la Carte. Vous devez donc
-		attendre la fin de cette génération qui peut prendre plusieurs minutes. Pour mettre à jour la liste et
-		le statut de votre carte, cliquez sur le bouton « Mettre à jour la liste ».
+		Ce mode de jeu vous permet de jouer, d’explorer, de construire ou réaliser des simulations d’aléas climatiques seul(e) sur une carte en 3D.
+		Dès lors que vous avez commandé une carte via le GAR ou Eduthèque elle s’affiche dans la liste des cartes de l’onglet « Partie solo ». La carte passe par plusieurs états avant de pouvoir être lancée.
+		Si la carte est « <style color=orange>en préparation</style> » cela veut dire que les services IGN sont en train de la générer avec toutes les données et options que vous avez choisi sur le site Minetest à la carte.
+		Vous devez donc attendre la fin de cette génération qui peut prendre plusieurs minutes. Pour mettre à jour la liste et le statut de votre carte, cliquez sur le bouton « Mettre à jour la liste ».
 		Si la carte est « <style color=yellow>prête</style> » cela veut dire que vous pouvez la télécharger depuis les serveurs IGN.
-		Sélectionnez la carte dans la liste puis cliquez sur le bouton « Télécharger ». La carte est téléchargée
-		et installée automatiquement sur votre poste. Le jeu vous demande ensuite de lui donner un nom
-		pour finaliser l’installation.
-		Si la carte est « <style color=#00ff00>installée</style> » cela veut dire que celle-ci est installée sur votre poste et prête à être
-		utilisée. Cliquez sur la carte dans la liste puis sur le bouton « Choisir cette carte » ou double-cliquez
-		sur la carte dans la liste pour passer à l’étape suivante.
+		Sélectionnez la carte dans la liste puis cliquez sur le bouton « Télécharger ». La carte est téléchargée et installée automatiquement sur votre poste. Le jeu vous demande ensuite de lui donner un nom pour finaliser l’installation.
+		Si la carte est « <style color=#00ff00>installée</style> » cela veut dire que celle-ci est installée sur votre poste et prête à être utilisée.
+		Cliquez sur la carte dans la liste puis sur le bouton « Choisir cette carte » ou double-cliquez sur la carte dans la liste pour passer à l’étape suivante.
 
 		<b>Partie solo – Choix de la carte II</b>
 
 		Une fois que vous avez choisi votre carte un deuxième écran apparaît. Plusieurs options s’offrent à vous :
-		« Jouer en 3D » : Ce bouton lancera le jeu IGN-kidscode. Vous apparaîtrez au sein de la carte 3D avec
-		votre avatar et un tutoriel vous accueillera dans le jeu pour vous expliquer les bases du logiciel.
-		« Cartographier en 2D » : Ce bouton lancera l’outil de cartographie IGN-kidscode. Une nouvelle
-		fenêtre va s’ouvrir et votre carte 2D va apparaître au bout de quelques secondes.
+		« Jouer en 3D » : Ce bouton lancera le jeu IGN-kidscode. Vous apparaîtrez au sein de la carte 3D avec votre avatar et un tutoriel vous accueillera dans le jeu pour vous expliquer les bases du logiciel.
+		« Cartographier en 2D » : Ce bouton lancera l’outil de cartographie IGN-kidscode. Une nouvelle fenêtre va s’ouvrir et votre carte 2D va apparaître au bout de quelques secondes.
 	]]},
 
 	{"Créer partie multijoueur", [[
-		Ce mode de jeu reprend les mêmes principes que le mode solo en ce qui concerne la liste des cartes,
-		leur mise à jour, leur téléchargement et leur installation. Toute carte qui se trouve dans la liste du
-		mode solo se trouvera également dans la liste des mondes disponibles pour le mode multijoueur.
-		En mode multijoueur une personne (la plupart du temps l’enseignant) sélectionne une carte et doit
-		ensuite héberger un serveur pour permettre aux élèves de se connecter au sein du même monde.
+		Ce mode de jeu reprend les mêmes principes que le mode solo en ce qui concerne la liste des cartes, leur mise à jour, leur téléchargement et leur installation.
+		Toute carte qui se trouve dans la liste du mode solo se trouvera également dans la liste des mondes disponibles pour le mode multijoueur.
+		En mode multijoueur une personne (la plupart du temps l’enseignant) sélectionne une carte et doit ensuite héberger un serveur pour permettre aux élèves de se connecter au sein du même monde.
 	]]},
 
 	{"Rejoindre partie multijoueur", [[
 		Lorsqu’un élève lance le jeu il doit cliquer sur l’onglet « Rejoindre une partie multijoueur ».
-		Si un serveur a été correctement lancé en suivant les étapes du chapitre « Créer partie
-		multijoueur » les élèves le verront affiché dans la liste des serveurs disponible.
-		L’élève doit d’abord choisir un pseudonyme puis cliquer sur le serveur précédemment
-		démarré par l’enseignant(e). Le serveur s’affichera comme ceci :
+		Si un serveur a été correctement lancé en suivant les étapes du chapitre « Créer partie multijoueur » les élèves le verront affiché dans la liste des serveurs disponible.
+		L’élève doit d’abord choisir un pseudonyme puis cliquer sur le serveur précédemment démarré par l’enseignant(e). Le serveur s’affichera comme ceci :
 		Nom de la carte + pseudonyme/nom de l’enseignant.
 		L’élève n’a plus qu’à cliquer sur « Rejoindre la partie » pour se connecter au monde.
 	]]},
